7a0f06bea6da2577275f6da162eea929d501319a
ArtLordMordt <manuelospino848@gmail.com>
2025-02-11 17:48:20 -0500

    Delete script (3).js

--- script (3).js
+++
@@ -1,4 +0,0 @@
-document.getElementById('buzon').addEventListener('click', function() {
-    const formSection = document.getElementById('sugerencias');
-    formSection.scrollIntoView({ behavior: 'smooth' });
-});
