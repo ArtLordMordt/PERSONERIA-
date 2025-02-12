b81a6933378cab766cbb1019b71e21601948e7cd
ArtLordMordt <manuelospino848@gmail.com>
2025-02-12 13:38:54 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,43 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Función para cambiar de sección
-  function cambiarSeccion(seccionId) {
-    const secciones = document.querySelectorAll('.seccion');
 
-    // Ocultar todas las secciones
-    secciones.forEach(seccion => seccion.classList.remove('active'));
-
-    // Mostrar la sección seleccionada
-    const seccionSeleccionada = document.getElementById(seccionId);
-    if (seccionSeleccionada) {
-      seccionSeleccionada.classList.add('active');
-    }
-  }
-
-  // Agregar eventos a los botones de navegación
-  const botonesNavegacion = document.querySelectorAll('nav ul li a[href^="#"]');
-  botonesNavegacion.forEach(boton => {
-    boton.addEventListener('click', function(event) {
-      event.preventDefault();
-      const seccionId = this.getAttribute('href').substring(1);
-      cambiarSeccion(seccionId);
-    });
+// Función para cambiar de sección
+function cambiarSeccion(seccionId) {
+  // Ocultar todas las secciones
+  const secciones = document.querySelectorAll('.seccion');
+  secciones.forEach(seccion => {
+    seccion.classList.remove('active');
   });
 
-  // Asegurar que al cargar la página, la sección activa sea 'inicio'
-  cambiarSeccion('inicio');
+  // Mostrar la sección seleccionada
+  const seccionSeleccionada = document.getElementById(seccionId);
+  seccionSeleccionada.classList.add('active');
+}
 
-  // Función para abrir el buzón de sugerencias en una nueva pestaña
-  function abrirBuzon() {
-    const url = 'https://forms.gle/rz1S5oraKyqbgjuk6';
-    window.open(url, '_blank');
-  }
+// Función para abrir el buzón de sugerencias
+function abrirBuzon() {
+  // Abrir el formulario de Google en una nueva pestaña
+  const url = 'https://forms.gle/rz1S5oraKyqbgjuk6';
+  window.open(url, '_blank');
+}
 
-  // Agregar evento al botón de buzón de sugerencias
-  const botonBuzon = document.getElementById('buzonBtn');
-  if (botonBuzon) {
-    botonBuzon.addEventListener('click', function(event) {
-      event.preventDefault();
-      abrirBuzon();
-    });
-  }
+// Agregar eventos a los botones de navegación
+const botonesNavegacion = document.querySelectorAll('nav ul li a');
+botonesNavegacion.forEach(boton => {
+  boton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const seccionId = this.getAttribute('href').substring(1);
+    cambiarSeccion(seccionId);
+  });
+});
+
+// Agregar evento al botón de buzón de sugerencias
+const botonBuzon = document.getElementById('buzonBtn');
+botonBuzon.addEventListener('click', function(event) {
+  event.preventDefault();
+  abrirBuzon();
 });
